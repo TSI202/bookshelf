5a51426992065bfd6992fc140f15aa81e21e8400
Dimitry Fedotov <dima@fedoto.ws>
2019-01-03 20:56:40 +0200

    book creation (ajax)

--- src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/CreateBookBean.java
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/CreateBookBean.java
@@ -2,17 +2,33 @@ package lv.tsi.javacourses.bookshelf.books.boundary;
 
 import lv.tsi.javacourses.bookshelf.books.model.BookEntity;
 
+import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.io.Serializable;
 
 @ViewScoped
 @Named
 public class CreateBookBean implements Serializable {
+    @PersistenceContext
+    private EntityManager em;
     private BookEntity book = new BookEntity();
+    private boolean created = false;
 
-    public void createBook() {
-        System.out.println("CREATING BOOK");
+    @Transactional
+    public String createBook() {
+        em.persist(book);
+        book = new BookEntity();
+        created = true;
+//        return "book-created.xhtml";
+        return null;
+    }
+
+    public boolean isCreated() {
+        return created;
     }
 
     public BookEntity getBook() {
