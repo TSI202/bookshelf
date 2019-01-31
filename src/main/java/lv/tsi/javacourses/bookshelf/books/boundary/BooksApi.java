ef8a9ab50e4a7b808c10446427c1c46f1133668a
Dimitry Fedotov <dima@fedoto.ws>
2019-01-31 18:28:10 +0200

    first rest service

---
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/BooksApi.java
@@ -0,0 +1,27 @@
+package lv.tsi.javacourses.bookshelf.books.boundary;
+
+import lv.tsi.javacourses.bookshelf.books.model.BookEntity;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/books")
+@Stateless
+public class BooksApi {
+    @PersistenceContext
+    private EntityManager em;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<BookEntity> getBooks() {
+        return em.createQuery("select b from Book b")
+                .getResultList();
+    }
+
+}
